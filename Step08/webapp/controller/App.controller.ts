ef36a27bc0f8eb3046054d093e4ab7742bbcf42e
GaboWin11 <gabriel.gomez@swisskrono.com>
2025-05-03 21:13:10 +0200

    Language selector with buttons

--- Step08/webapp/controller/App.controller.ts
+++ Step08/webapp/controller/App.controller.ts
@@ -2,23 +2,23 @@ import MessageToast from "sap/m/MessageToast";
 import Controller from "sap/ui/core/mvc/Controller";
 import JSONModel from "sap/ui/model/json/JSONModel";
 import ResourceModel from "sap/ui/model/resource/ResourceModel";
-import ResourceBundle from "sap/base/i18n/ResourceBundle";
+import Event from "sap/ui/base/Event";
 
 /**
  * @name ui5.walkthrough.controller.App
  */
 export default class AppController extends Controller {
-   onInit() : void {
-      // set data model on view
-        const data = {
-           recipient: {
-              name: "World"
-           }
-        };
-        const dataModel = new JSONModel(data);
-        this.getView()?.setModel(dataModel);
-      
-   // set i18n model on view
+   onInit(): void {
+      // Set data model on view
+      const data = {
+         recipient: {
+            name: "World"
+         }
+      };
+      const dataModel = new JSONModel(data);
+      this.getView()?.setModel(dataModel);
+
+      // Set i18n model
       const i18nModel = new ResourceModel({
          bundleName: "ui5.walkthrough.i18n.i18n"
       });
@@ -33,7 +33,21 @@ export default class AppController extends Controller {
       // show message
       MessageToast.show(msg);
    }
+
    formatDescription(helloMsg: string, recipientName: string): string {
       return helloMsg.replace("{0}", recipientName);
    }
-};
+
+   onChangeLanguage(oEvent: Event): void {
+      // Get the selected language from the button's custom data
+      const customData = oEvent.getSource()?.getCustomData();
+      const selectedLanguage = customData?.find(data => data.getKey() === "key")?.getValue();
+      if (selectedLanguage) {
+         const i18nModel = new ResourceModel({
+            bundleName: "ui5.walkthrough.i18n.i18n",
+            bundleLocale: selectedLanguage
+         });
+         this.getView()?.setModel(i18nModel, "i18n");
+      }
+   }
+}
