e7d8c2c328d7415104813b085ad39b33366d07a0
GaboWin11 <gabriel.gomez@swisskrono.com>
2025-05-11 22:02:27 +0200

    step eightteenth, where the fragment offers an icon for a hello globe walkthrough

---
+++ Step18/webapp/controller/App.controller.ts
@@ -0,0 +1,12 @@
+import MessageToast from "sap/m/MessageToast";
+import Controller from "sap/ui/core/mvc/Controller";
+import JSONModel from "sap/ui/model/json/JSONModel";
+import ResourceModel from "sap/ui/model/resource/ResourceModel";
+import Event from "sap/ui/base/Event";
+
+/**
+ * @name ui5.walkthrough.controller.App
+ */
+export default class AppController extends Controller {
+
+};
