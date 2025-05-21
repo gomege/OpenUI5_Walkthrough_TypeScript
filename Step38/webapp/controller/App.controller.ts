8dc1784fecc44c204c6cceba9351965811feced6
GaboWin11 <gabriel.gomez@swisskrono.com>
2025-05-21 22:39:41 +0200

    step thirty eight, build project

---
+++ Step38/webapp/controller/App.controller.ts
@@ -0,0 +1,14 @@
+import MessageToast from "sap/m/MessageToast";
+import Controller from "sap/ui/core/mvc/Controller";
+import JSONModel from "sap/ui/model/json/JSONModel";
+import ResourceModel from "sap/ui/model/resource/ResourceModel";
+import Event from "sap/ui/base/Event";
+
+/**
+ * @name ui5.walkthrough.controller.App
+ */
+export default class AppController extends Controller {
+    onInit(): void  {
+       this.getView().addStyleClass((<Component> this.getOwnerComponent()).getContentDensityClass())
+    }
+};
